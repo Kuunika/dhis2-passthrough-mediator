6d768c1c63038f28da8f3aa299514d8a61f87dfb
Brett Larry Onions <46473797+brett-onions@users.noreply.github.com>
2021-10-27 18:03:59 +0200

    added env file

--- build_and_run.sh
+++ build_and_run.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 docker build -t dhis-2-passthrough-mediator .
-docker run --name passthrough-mediator -p 3434:3434 --network openhimv7_openhim dhis-2-passthrough-mediator
+docker run --name passthrough-mediator --env-file .env -p 3434:3434 --network openhimv7_openhim dhis-2-passthrough-mediator
