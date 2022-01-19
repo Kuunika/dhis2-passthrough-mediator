d386d94a686878c7b8f8819de567b7fa28153bb7
Brett Larry Onions <46473797+brett-onions@users.noreply.github.com>
2022-01-19 09:51:06 +0200

    enabling cors

--- src/main.ts
+++ src/main.ts
@@ -5,6 +5,13 @@ import { API_GLOBAL_BASE_URL, PORT, DEFAULT_PORT } from './common/constants';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.enableCors({
+    origin: true,
+    methods: 'GET, POST, PUT, HEAD, OPTIONS',
+    preflightContinue: false,
+    optionsSuccessStatus: 204,
+    credentials: true,
+  });
   const configService = app.get<ConfigService>(ConfigService);
   const globalPrefix = configService.get<string>(API_GLOBAL_BASE_URL);
   const port = configService.get<number>(PORT) || DEFAULT_PORT;
