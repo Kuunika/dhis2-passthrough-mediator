9f86b559d3ca3500bb66e6c077db20d14e215e24
Brett Larry Onions <46473797+brett-onions@users.noreply.github.com>
2022-01-20 15:12:48 +0200

    adding Content-Security-Policy

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,22 +1,24 @@
-import { Controller, Get, Post, Req, Res } from '@nestjs/common';
+import { Controller, Get, Header, Post, Req } from '@nestjs/common';
 import { Request } from 'express';
 import { DHIS2Service } from './dhis2/dhis2.service';
 
 @Controller()
 export class AppController {
-	constructor(private readonly dhis2Service: DHIS2Service) { }
+  constructor(private readonly dhis2Service: DHIS2Service) {}
 
-	@Get(`*`)
-	async catchAllGet(@Req() request: Request) {
-		return this.dhis2Service.requestServiceFromDHIS2('GET', request.url);
-	}
+  @Get(`*`)
+  @Header('Content-Security-Policy', "default-src 'self' 'unsafe-inline'")
+  async catchAllGet(@Req() request: Request) {
+    return this.dhis2Service.requestServiceFromDHIS2('GET', request.url);
+  }
 
-	@Post(`*`)
-	async catchAllPost(@Req() request: Request) {
-		return this.dhis2Service.requestServiceFromDHIS2(
-			'POST',
-			request.url,
-			request.body,
-		);
-	}
+  @Post(`*`)
+  @Header('Content-Security-Policy', "default-src 'self' 'unsafe-inline'")
+  async catchAllPost(@Req() request: Request) {
+    return this.dhis2Service.requestServiceFromDHIS2(
+      'POST',
+      request.url,
+      request.body,
+    );
+  }
 }
